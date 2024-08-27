9a0fc2fba3efd0d62e776fdfdfffc33010884b8e
Satish Rathod <satishrathod.dev@gmail.com>
2024-08-27 10:05:30 +0530

    finished

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,8 @@ import axios from "axios";
 const fetchURL = "https://dummyjson.com/products";
 
 const App = () => {
-  const [post, setPost] = useState(null);
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     // const getData = axios.get(fetchURL).then((response) => {
@@ -17,9 +18,10 @@ const App = () => {
     const getData = async () => {
       try {
         const response = await axios.get(fetchURL);
-        setPost(response.data);
-        console.log(response.data);
+        setData(response.data);
+        setError(null); // Clear any previous errors
       } catch (error) {
+        setError("An error occurred while fetching data.");
         console.error("Error fetching data:", error);
       }
     };
@@ -31,9 +33,25 @@ const App = () => {
   // };
 
   // if (!post)
+  if (error) {
+    return (
+      <div>
+        <pre>{error}</pre>
+      </div>
+    );
+  }
+
+  if (!data) {
+    return (
+      <div>
+        <pre>Loading...</pre>
+      </div>
+    );
+  }
+
   return (
     <div>
-      <pre>{post ? JSON.stringify(post, null, 2) : "Loading..."}</pre>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   );
 };
